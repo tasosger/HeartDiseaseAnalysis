3fea4870842da00b21466542ad4503c5e9935be5
tasosger <tassos.gerassis@gmail.com>
2024-07-02 00:35:22 +0300

    id3

--- decisionTree.py
+++ decisionTree.py
@@ -1,7 +1,7 @@
-from typing import List, Any
+from typing import List, Any, Dict, TypeVar, NamedTuple, Union
 import math
 from supplementaries import Vector
-from collections import Counter
+from collections import Counter, defaultdict
 
 def entropy(class_probabilities: Vector)->float:
     return sum(-p*math.log(p,2) for p in class_probabilities if p>0)
@@ -13,3 +13,57 @@ def class_probabilities(labels: List[Any])->List[float]:
 def data_entropy(labels: List[Any]) -> float:
     return entropy(class_probabilities(labels))
 
+def partition_entropy(subsets: List[List[Any]])-> float:
+    total_count = sum(len(subset) for subset in subsets)
+    return sum(data_entropy(subset)* len(subset)/total_count for subset in subsets)
+
+T = TypeVar('T')
+
+def partition_by(inputs: List[T], attribute: str)-> Dict[Any,List[T]]:
+    partitions: Dict[Any, List[T]] = defaultdict(list)
+    for input in inputs:
+        key = getattr(input, attribute)
+        partitions[key].append(input)
+    return partitions
+def partition_entropy_by(inputs: List[Any], attribute: str, label_attribute:str)->float:
+    partitions = partition_by(inputs,attribute)
+    labels = [[getattr(input,label_attribute) for input in partition] for partition in partitions]
+    return partition_entropy(labels)
+
+class Leaf(NamedTuple):
+    value: Any
+class Split(NamedTuple):
+    attribute: str
+    subtrees: dict
+    default_value: Any =None
+
+DecisionTree = Union[Leaf, Split]
+
+
+def classify(tree: DecisionTree, input: Any)-> Any:
+    if isinstance(tree, Leaf):
+        return tree.value
+    
+    subtree_key = getattr(input, tree.attribute)
+
+    if subtree_key not in tree.subtrees:
+        return tre.default_value
+     
+    subtree  = tree.subtrees[subtree_key]
+    return classify(subtree, inputs)
+def build_tree_id3(inputs: List[Any], split_attributes: List[str], target_attribute: str)-> DecisionTree:
+    label_counts = Counter(getattr(input, target_attributes) for input in inputs)
+    most_common_label = label_counts.most_common(1)[0][0]
+
+    if len(label_counts) ==1:
+        return Leaf(most_common_label)
+    if not split_attributes:
+        return Leaf(most_common_label)
+    def split_entropy(attribute: str)-> float:
+        return partition_entropy_by(inputs, attribute,target_attribute)
+    best_attribute = min(split_attributes, key = split_entropy)
+    new_attributes = [a for a in split_attributes if a!= best_attribute]
+    partitions =partition_by(inputs,best_attribute)
+    subtrees = {attribute_value: build_tree_id3(subset, new_attributes, target_attribute) for attribute_value, subset in partition.items() }
+    return Split(best_attribute, subtrees, default_value=most_common_label)
+    
